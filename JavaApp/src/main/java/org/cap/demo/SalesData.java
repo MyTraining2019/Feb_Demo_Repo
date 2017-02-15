c74755712281442dcd2273731a0d06c3071e5a53
caprepo <vijayalakshmi.david@capgemini.com>
2017-02-15 16:38:55 +0530

    Commit

---
+++ JavaApp/src/main/java/org/cap/demo/SalesData.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class SalesData {
+	
+	public void displayData(){
+		
+		System.out.println("Display Data");
+		int[] myArr={1,67,90,34,0};
+		
+		for(int i=0;i<myArr.length;i++)
+			System.out.println(myArr[i]);
+	}
+
+}
