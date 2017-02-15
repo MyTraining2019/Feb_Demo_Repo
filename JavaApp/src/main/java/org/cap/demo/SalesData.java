d989417dfb5183d86e08b4f686f2966c60afc39b
caprepo <vijayalakshmi.david@capgemini.com>
2017-02-15 16:51:06 +0530

    Sales Date Modified

--- JavaApp/src/main/java/org/cap/demo/SalesData.java
+++ JavaApp/src/main/java/org/cap/demo/SalesData.java
@@ -5,6 +5,7 @@ public class SalesData {
 	public void displayData(){
 		
 		System.out.println("Display Data");
+		System.out.println("Hey This is Sales Data");
 		int[] myArr={1,67,90,34,0};
 		
 		for(int i=0;i<myArr.length;i++)
