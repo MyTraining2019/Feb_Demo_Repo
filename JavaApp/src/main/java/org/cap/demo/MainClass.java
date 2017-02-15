32bb502c70824475fef48228c5f3ab59dfdeed15
caprepo <vijayalakshmi.david@capgemini.com>
2017-02-15 17:08:02 +0530

    commit

---
+++ JavaApp/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,17 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		SalesData salesData=new SalesData();
+		salesData.displayData();
+		greetings();
+	}
+	
+	
+	public static void greetings(){
+		System.out.println("Hello! Good Evening!");
+	}
+
+}
---
+++ JavaApp/src/main/java/org/cap/demo/SalesData.java
@@ -0,0 +1,20 @@
+package org.cap.demo;
+
+public class SalesData {
+	
+	public void displayData(){
+		
+		System.out.println("Display Data..");
+		System.out.println("Hey This is Sales Data");
+		int[] myArr={1,67,90,34,0};
+		
+		for(int i=0;i<myArr.length;i++)
+			System.out.println(myArr[i]);
+	}
+	
+	public void printMessage(){
+		System.out.println("Greetings to All!");
+		System.out.println("Welcome To Sales Report!");
+	}
+
+}
